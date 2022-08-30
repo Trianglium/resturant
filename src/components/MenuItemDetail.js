3b5c3eff7a71f476a6f7f750e9404ecb710a5750
Jordan <jbrockerrudow@gmail.com>
2022-08-30 16:33:23 -0500

    Initialize MenuItemDetail Component

---
+++ src/components/MenuItemDetail.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class MenuItemDetail extends Component {
+
+}
+
+
+export default MenuItemDetail;

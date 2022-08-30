42e6c5b2075e5493b211db123ef6e52573986271
Jordan <jbrockerrudow@gmail.com>
2022-08-30 14:00:37 -0500

    Add Party Menu Items and Mock Data for Menu Items

--- src/shared/menuItems.js
+++ src/shared/menuItems.js
@@ -1,186 +1,227 @@
 export const MENUITEMS =
     [
         {
-        id: 0,
-        name:'Uthappizza',
-        image: 'assets/images/uthappizza.png',
-        category: 'mains',
-        label:'Hot',
-        price:'4.99',
-        description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-        comments: [
-            {
-            id: 0,
-            rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
-            author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
+            "Salads":{
+                "description":"Choice of dressing: ranch, thousand island, French creamy garlic, Italian, low fat Italian, caesar, honey mustard.",
+                "Garden Salad":4.85,
+                "Greek Salad":6.19,
+                "Chicken Salad":6.19,
+                "Chicken Caesar Salad":6.19
             },
-            {
-            id: 1,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
+            "Gyros":{
+                "description":"Fresh sliced to order.",
+                "Gyros Sandwich":5.59,
+                "Chicken Gyros Sandwich":6.20,
+                "Jumbo Gyros Plate":10.99,
+                "Jumbo Chicken Gyros Plate":11.99,
+                "Fiesta Gyros":6.29,
+                "Fiesta Chicken Gyros":6.59,
+                "Fiesta Gyros Plate":11.99,
+                "Fiesta Chicken Gyros Plate":12.49
             },
-            {
-            id: 2,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
+            "Beef":{
+                "description":"Homemade recipe!",
+                "Italian Beef Sandwich":5.49,
+                "Italian Sausage Sandwich":4.05,
+                "Combo Beef and Sausage Sandwich":6.92,
+                "Philly Cheese Steak":5.69,
+                "PoorBoy Steak":5.05,
+                "Beef Wrap":5.09,
+                "Pork Tenderloin Sandwich":6.69
             },
-            {
-            id: 3,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
+            "Panini":{
+                "description":"Italian grilled! Choice of Swiss, cheddar, American or mozzarella cheese.",
+                "Smoked Turkey and Cheese Panini":5.05,
+                "Chicken and Cheese Panini":5.15,
+                "Cheese Panini":2.99,
+                "Turkey Bacon Avocado Panini":5.99,
+                "Chicken Ranchero Panini":5.99,
+                "BLT Panini":4.29,
+                "BLT Sandwich":3.29
             },
-            {
-            id: 4,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
+            "Burgers":{
+                "description":"Served with ketchup mustard, pickle, lettuce, tomatoes and onions.",
+                "Jr Hamburger":1.99,
+                "Hamburger":3.19,
+                "Cheeseburger":3.95,
+                "Double Hamburger":4.99,
+                "Double Cheeseburger":5.19,
+                "Mushroom Swiss Burger":4.99,
+                "Mega Burger":5.99,
+                "Jalapeno Cheddar Burger":4.99,
+                "Guacamole Bacon Cheeseburger":5.29,
+                "BBQ Bacon Cheeseburger":5.29,
+                "Patty Melt On Rye":4.39
+            },
+            "Hot Dogs":{
+                "description":"Chicago style pure beef! Served with mustard, onions, relish, pickle and tomatoes.",
+                "Hot Dog":2.25,
+                "Hot Dog with fries":3.55,
+                "Polish Sausage":2.99,
+                "Polish Sausage with Fries":4.25,
+                "Chili Dog":2.99,
+                "Cheese Dog":2.89,
+                "Chili Cheese Dog":3.19,
+                "Corn Dog":2.15
+            },
+            "Chicken and Fish":{
+                "Grilled Chicken Sandwich":4.95,
+                "Breaded Chicken Sandwich":4.95,
+                "Mega Chicken Sandwich":6.39,
+                "Chicken Caesar Pita":4.99,
+                "Chicken Strips 3 Pcs":3.69,
+                "Chicken Strips 5 Pcs":5.15,
+                "Chicken Wrap":4.65,
+                "Chicken Parmesan":5.75,
+                "Grilled Cheese":1.99,
+                "Fish Sandwich":3.99
+            },
+            "Tacos and Burritos":{
+                "description":"Our specialty!",
+                "Beef or Chicken Taco":2.15,
+                "Beef or Chicken Burrito":6.40,
+                "Bean Burrito":4.25,
+                "Super Taco On Pita":5.99,
+                "Quesadilla Steak or Chicken":6.25,
+                "Nacho 1 2 Plate":4.99,
+                "Nacho Full Plate":6.89
+            },
+            "Dinners":{
+                "Beef Taco Dinner 3 Pcs":7.49,
+                "Burrito Dinner Steak or Chicken":7.49,
+                "Quesadilla Dinner Steak or Chicken":7.29,
+                "Fajita Dinner Steak or Chicken":8.45,
+                "Fried Chicken 4 Pcs":7.89,
+                "Shrimp Basket 21 25 Pcs":6.45,
+                "Cod Dinner":7.79,
+                "Fish and Chips":7.99
+            },
+            "Kids Menu":{
+                "description":"Includes fries and pop.",
+                "Hamburger Kids":3.85,
+                "Grilled Cheese Kids":3.85,
+                "Corn Dog Kids":3.85,
+                "Hot Dog Kids":3.85,
+                "Taco Kids":3.85,
+                "Chicken Strips Kids":3.85
+            },
+            "Sides":{
+                "Chips":1.29,
+                "Chips and Guacamole":2.49,
+                "Chips and Salsa":2.60,
+                "Guacamole":0.99,
+                "Sour Cream":0.55,
+                "Rice":1.90,
+                "Beans":1.90,
+                "Small Fries":1.65,
+                "Small Fries with Cheese":2.30,
+                "Large Fries":1.99,
+                "Large Fries with Cheese":2.64,
+                "Mozzarella Sticks":4.25,
+                "Jalapenos Poppers":3.95,
+                "Onion Rings":3.15,
+                "Mushrooms":3.25,
+                "Pizza Puff":3.05,
+                "Fried Pickles":4.45
+            },
+            "Desserts":{
+                "Edy s Sundaes":3.99,
+                "Ice Cream Cone":2.45,
+                "2 Scoop Ice Cream":3.80,
+                "Waffle Cone":3.99,
+                "Banana Splits":6.15
+            },
+            "Beverages":{
+                "Soda":{
+                    "options":[
+                        "pepsi",
+                        "diet pepsi",
+                        "cherry pepsi",
+                        "sierra mist",
+                        "mug rootbeer",
+                        "dr. pepper",
+                        "mountain dew",
+                        "pink lemonade",
+                        "ice tea",
+                        "raspberry ice tea."
+                    ],
+                    "sizes":{
+                        "small":1.45,
+                        "medium":1.85,
+                        "large":2.25
+                    }
+                },
+                "Shakes":{
+                    "options":[
+                        "chocolate",
+                        "vanilla",
+                        "strawberry"
+                    ],
+                    "sizes":{
+                        "small":2.99,
+                        "medium":3.89
+                    }
+                },
+                "Fresh Squeezed Lemonade":{
+                    "options":[
+                        "lemonade"
+                    ],
+                    "sizes":{
+                        "small":1.55,
+                        "medium":2.05,
+                        "large":2.60
+                    }
+                },
+                "Bottled Water":1.25
             }
-        ]                        },
-        {
-        id: 1,
-        name:'Zucchipakoda',
-        image: 'assets/images/zucchipakoda.png',
-        category: 'appetizer',
-        label:'',
-        price:'1.99',
-        description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-        comments: [
-            {
-            id: 0,
-            rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
-            author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
-            },
-            {
-            id: 1,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-            id: 2,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-            id: 3,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
-            },
-            {
-            id: 4,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
-            }
-        ]
-        },
-        {
-        id: 2,
-        name:'Vadonut',
-        image: 'assets/images/vadonut.png',
-        category: 'appetizer',
-        label:'New',
-        price:'1.99',
-        description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-        comments: [
-            {
-            id: 0,
-            rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
-            author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
-            },
-            {
-            id: 1,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-            id: 2,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-            id: 3,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
-            },
-            {
-            id: 4,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
-            }
-        ]
-        },
-        {
-        id: 3,
-        name:'ElaiCheese Cake',
-        image: 'assets/images/elaicheesecake.png',
-        category: 'dessert',
-        label:'',
-        price:'2.99',
-        description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-        comments: [
-            {
-            id: 0,
-            rating: 5,
-            comment: "Imagine all the eatables, living in conFusion!",
-            author: "John Lemon",
-            date: "2012-10-16T17:57:28.556094Z"
-            },
-            {
-            id: 1,
-            rating: 4,
-            comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
-            author: "Paul McVites",
-            date: "2014-09-05T17:57:28.556094Z"
-            },
-            {
-            id: 2,
-            rating: 3,
-            comment: "Eat it, just eat it!",
-            author: "Michael Jaikishan",
-            date: "2015-02-13T17:57:28.556094Z"
-            },
-            {
-            id: 3,
-            rating: 4,
-            comment: "Ultimate, Reaching for the stars!",
-            author: "Ringo Starry",
-            date: "2013-12-02T17:57:28.556094Z"
-            },
-            {
-            id: 4,
-            rating: 2,
-            comment: "It's your birthday, we're gonna party!",
-            author: "25 Cent",
-            date: "2011-12-02T17:57:28.556094Z"
-            }
-        ]
         }
     ];
+
+export const PARTYMENUITEMS = [
+    {
+        "Taste Catering": {
+            "description": "Serves 20-25 people.",
+            "Combo 1 Party": {
+                "description": "5 lbs. Italian beef with gravy, hot and sweet peppers 5 loaf bread.",
+                "price":79.99
+            },
+            "Combo 2 Party": {
+                "description": "20 Italian sausages, 5 loafs bread, hot and sweet peppers, red sauce.",
+                "price":59.99
+            },
+            "Combo 3 Party": {
+                "description": "6 lbs. gyros meat with sauce, 20 pita bread, onions and tomatoes.",
+                "price":79.99,
+            "Combo 4 Party": {
+                "description": "6 lbs. Chicken gyros with sauce, 20 pita bread, onions and tomatoes. Homemade hot sauce (red or green).",
+                "price":89.99
+            },
+            "Combo 5 Party": {
+                "description": "50 tacos steak, chicken or beef, flour or corn tortillas, tray of Mexican rice, tray of refried beans.",
+                "price":99.99
+            },
+        },
+        
+        "By The Pounds": {
+            "Italian Beef with Gravy Party": 8.99,
+            "Gyros with Sauce Party": 8.99,
+            "Chicken Gyros with Sauce Party": 9.99,
+        },
+        "Sides": {
+            "1 Loaf Bread Party": 3.50,
+            "Side of Hot Peppers Party": 3.95,
+            "Side of Sweet Peppers Party": 3.75,
+            "Side of Red Sauce Party": 3.75,
+            "Side of Gyros Sauce Party": 3.75,
+            "Pita Bread Party": 0.85,
+            "Chips and Guacamole Platter Party": 27.99,
+            "Chips and Salsa Platter Party": 24.99,
+            "Tray of Guacamole Party": 18.99,
+            "Tray of Refried Beans Party": 19.99,
+            "Tray of Garden Salad Party": 19.99,
+            "Tray of Mexican Rice Party": 19.99,
+            "Tray of Greek Salad Party": 27.99
+        }
+    }
+
+];

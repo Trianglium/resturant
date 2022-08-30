1daa5434e119b00a009cfc3f77e29d045ab81677
Jordan <jbrockerrudow@gmail.com>
2022-08-30 16:41:19 -0500

    Make use of Main component in app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/Menu';
-import { MENUITEMS } from './shared/menuItems';
-
+import Main from './components/Main';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      menuitems: MENUITEMS
-    };
-  }
+
   render() {
     return (
       <div className="App">
-        <><Navbar>
-          <div className="container">
-            <NavbarBrand href="/"></NavbarBrand>
-          </div>
-        </Navbar><div className="container">
-            <Menu dishes={this.state.menuitems} />
-          </div></>
+        <Main />
       </div>
     );
   }

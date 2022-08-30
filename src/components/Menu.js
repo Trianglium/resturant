daf396c6b95bd5e08cdf91e30816be1a4502c9cd
Jordan <jbrockerrudow@gmail.com>
2022-08-30 16:41:09 -0500

    Update Menu to be FBC

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,34 +1,24 @@
-import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import MenuItemDetail from './components/MenuItemDetail';
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-
-class Menu extends Component {
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            selectedDish: null,
-        }
+    function RenderMenuItem ({dish, onClick}) {
+        return (
+            <Card
+                onClick={() => onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        );
     }
 
-    onDishSelect(dish) {
-        this.setState({
-            selectedDish: dish
-        });
-    }
+    const Menu = (props) => {
 
-    render() {
-        const menu = this.props.dishes.map((dish) => {
+        const menu = props.dishes.map((dish) => {
             return (
-                <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{dish.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
+                <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                    <RenderMenuItem dish={dish} onClick={props.onClick} />
                 </div>
             );
         });
@@ -38,10 +28,8 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <MenuItemDetail dish={this.state.selectedDish}/>
             </div>
         );
     }
-}
 
 export default Menu;

dfa131eaad2efe25a406057339cdbe5fb685aa1f
Jordan <jbrockerrudow@gmail.com>
2022-08-30 16:35:58 -0500

    Add render methods for MenuItem Data

--- src/components/MenuItemDetail.js
+++ src/components/MenuItemDetail.js
@@ -1,8 +1,70 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class MenuItemDetail extends Component {
+    renderDish(dish) {
+        if(dish !== null) {
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        } else {
+            return (
+                <div></div>
+            );
+        }
+    }
 
+    renderComments(comments) {
+        if(comments !== null && comments.length) {
+            const commentList = comments.map((comment)=>{
+                return(
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author}, {comment.date.toLocaleString()}</p>
+                    </li>
+                );
+            });
+
+            return(
+                <div>
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {commentList}
+                    </ul>
+                </div>
+            );
+        } else {
+            return (
+                <div></div>
+            );
+        }
+    }
+
+    render() {
+        if(this.props.dish !== null) {
+            return(
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderDish(this.props.dish)}
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        {this.renderComments(this.props.dish.comments)}
+                    </div>
+                </div>
+            );
+        } else {
+            return(
+                <div></div>
+            );
+        }
+        
+    }
 }
 
 

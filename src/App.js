44b3442e449bdae8b30fdfd7e4e4190a91f3f250
Jordan <jbrockerrudow@gmail.com>
2022-08-31 13:44:26 -0500

    Add Components and Configure Store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import { BrowserRouter } from 'react-router-dom';
+import Menu from './pages/Menu';
 import Main from './components/Main';
+import { DISHES } from './shared/dishes';
+
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <Provider store={store}>
+        <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    </Provider>
     );
   }
 }
+
 
 export default App;

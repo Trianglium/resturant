921cbd3f902907b8496e8aff6510bf245981113e
Jordan <jbrockerrudow@gmail.com>
2022-09-01 07:39:39 -0500

    Update JSX Casing

--- src/pages/MenuItemDetail.js
+++ src/pages/MenuItemDetail.js
@@ -150,20 +150,19 @@ class CommentForm extends Component{
                       <Row className="form-group">
                            <Label htmlFor="rating">Rating</Label>
                                 <Col md={12}>
-                                    <Control.select model=".rating" name="rating"
-                                 className="form-control">
+                                    <Control.Select model=".rating" name="rating" className="form-control">
                                          <option>1</option>
                                          <option>2</option>
                                          <option>3</option>
                                          <option>4</option>
                                          <option>5</option>
-                                    </Control.select>
+                                    </Control.Select>
                                   </Col>
                       </Row>
                        <Row className="form-group">
                           <Label htmlFor="author" md={2}>Your Name</Label>
                                  <Col md={10}>
-                                     <Control.text model=".author" id="author" name="author"
+                                     <Control.Text model=".author" id="author" name="author"
                                          placeholder="Your Name"
                                          className="form-control"
                                          validators={{
@@ -185,7 +184,7 @@ class CommentForm extends Component{
       	                			<Row className="form-group">
       	                             <Label htmlFor="comment">Comment</Label>
       	                             <Col md={12}>
-      	                                    <Control.textarea model=".comment" id="comment" name="comment"
+      	                                    <Control.Textarea model=".comment" id="comment" name="comment"
       	                                        rows="5"
       	                                        className="form-control" />
                                     	</Col>

c07a40afab4e5868c00f4da990ad8f52e985d4e2
Jordan <jbrockerrudow@gmail.com>
2022-09-02 12:06:15 -0500

    Add quotes to testimonials section and alter leader data

--- src/components/About.component.js
+++ src/components/About.component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './About.component.css';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Fade, Stagger } from 'react-animation-components';
@@ -18,20 +19,40 @@ function RenderLeader({leader, isLoading, errMess}) {
   }
   else if (leader != null) {
       return(
+
         <div key={leader.id} className="col-12 mt-5">
           <Stagger in>
             <Media tag="li">
               <Fade in>
-                <Media left middle>
-                    <Media object src={baseUrl + leader.image} alt={leader.name} />
-                </Media>
-                <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                </Media>
+                <div className="row">
+                    <div className="col-8">
+                        <Card>
+                            <CardBody className="bg-faded">
+                                <blockquote className="blockquote">
+                                    <p className="my-3 quote-review"><i class="fa fa-quote-left" aria-hidden="true"></i> Love the Taco Dinner! <i class="fa fa-quote-right" aria-hidden="true"></i></p>
+                                    <footer className="blockquote-footer">Yogi Berra,
+                                    <cite title="Source Title mt-2">The Wit and Wisdom of Yogi Berra,
+                                        P. Pepe, Diversion Books, 2014</cite>
+                                    </footer>
+                                </blockquote>
+                            </CardBody>
+                        </Card>
+                    </div>
+
+                    <div className="col-4">
+                    <Media>
+                        <Media object src={baseUrl + leader.image} alt={leader.name} />
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>{leader.name}</Media>
+                        <p>{leader.designation}</p>
+                    </Media>
+                        
+                    </div>
+                
+                </div>
               </Fade>
-            </Media>
+              </Media>
           </Stagger>
         </div>
       );
@@ -73,20 +94,6 @@ const About = (props) => {
                 <div className="col-12 col-md-5">
                     <Card>
                         <CardHeader className="bg-info text-white">Facts At a Glance</CardHeader>
-                    </Card>
-                </div>
-                <div className="col-12">
-                    <Card>
-                        <CardBody className="bg-faded">
-                            <blockquote className="blockquote">
-                                <p className="my-3 quote-review"><i class="fa fa-quote-left" aria-hidden="true"></i> You better cut the pizza in four pieces because
-                                    I'm not hungry enough to eat six. <i class="fa fa-quote-right" aria-hidden="true"></i></p>
-                                <footer className="blockquote-footer">Yogi Berra,
-                                <cite title="Source Title mt-2">The Wit and Wisdom of Yogi Berra,
-                                    P. Pepe, Diversion Books, 2014</cite>
-                                </footer>
-                            </blockquote>
-                        </CardBody>
                     </Card>
                 </div>
             </div>

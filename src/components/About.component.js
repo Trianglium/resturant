bf016225b77e95602b7ad668b1abbfc26d58a254
Jordan <jbrockerrudow@gmail.com>
2022-09-08 14:03:23 -0500

    Modularize AboutSection

---
+++ src/components/About.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function AboutSection(props) {
+    return(
+        <section className="about-section" id="about">
+        <article>
+          <h3>
+              The best tacos in town
+          </h3>
+          <p>
+            We take pride in being the go-to taco place while also offering other high quality food options for low prices.
+          </p>
+        </article>
+      </section>
+    )
+}
+
+export default AboutSection;

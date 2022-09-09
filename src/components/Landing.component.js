f363df764839eed0f382bf029953ce8835f05c19
Jordan <jbrockerrudow@gmail.com>
2022-09-09 08:27:35 -0500

    Add items to Slider Component and remove from Landing Page Component

--- src/components/Landing.component.js
+++ src/components/Landing.component.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Landing.component.css';
-import {itemSlides} from '../shared/foodSlides';
 import { FlexNav, ImageHeader } from './Header.component';
 import InfoFooter from './Footer.component';
 import AboutSection from './About.component';

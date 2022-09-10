95fe4066243e8d06f84c8166ee6545a516790665
Jordan <jbrockerrudow@gmail.com>
2022-09-10 11:30:57 -0500

    Add second article in about

--- src/components/About.component.js
+++ src/components/About.component.js
@@ -3,7 +3,15 @@ import React from 'react';
 function AboutSection(props) {
     return(
         <section className="about-section" id="about">
-        <article>
+        <article className="pe-4">
+          <h3>
+              The best tacos in town
+          </h3>
+          <p>
+            We take pride in being the go-to taco place while also offering other high quality food options for low prices.
+          </p>
+        </article>
+        <article className="pe-4">
           <h3>
               The best tacos in town
           </h3>

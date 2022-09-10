1f45eea125904de83b81c3785a46c2da83d29922
Jordan <jbrockerrudow@gmail.com>
2022-09-10 12:47:44 -0500

    Separate Food Images from all image assets

--- src/shared/foodSlides.js
+++ src/shared/foodSlides.js
@@ -1,37 +1,37 @@
 export const items = [
     {
         id: 1,
-        src: "assets/images/tacos-dinner-slide.jpg",
+        src: "assets/images/food/tacos-dinner-slide.jpg",
         altText: "Taco Dinner",
     },
     {
         id: 2,
-        src: "assets/images/chips-salsa-slide.jpg",
+        src: "assets/images/food/chips-salsa-slide.jpg",
         altText: "Chips and Salsa",
     },
     {
         id: 3,
-        src: "assets/images/salad-slide.jpg",
+        src: "assets/images/food/salad-slide.jpg",
         altText: "Salad",
     },
     {
         id: 4,
-        src: "assets/images/burgers-slide.jpg",
+        src: "assets/images/food/burgers-slide.jpg",
         altText: "Burger",
     },
     {
         id: 5,
-        src: "assets/images/chicken-slide.jpg",
+        src: "assets/images/food/chicken-slide.jpg",
         altText: "Chicken",
     },
     {
         id: 6,
-        src: "assets/images/tacos-corn-slide.jpg",
+        src: "assets/images/food/tacos-corn-slide.jpg",
         altText: "Corn Tacos",
     },
     {
         id: 7,
-        src: "assets/images/tacos-plate-slide.jpg",
+        src: "assets/images/food/tacos-plate-slide.jpg",
         altText: "Taco Plate",
     },
 ]
--- util/img/img_resize.py
+++ util/img/img_resize.py
@@ -1,3 +1,6 @@
 import os
+from pathlib import Path
+from PIL import Image
 
-cwd = os.getcwd()
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+IMAGES_DIR = os.path.join(os.path.join(os.path.join(os.path.join(BASE_DIR, 'public'), 'assets'), 'images'), 'food')

25cf74227b1c2a6ca8f1764e2231c78eb7c2233d
Jordan <jbrockerrudow@gmail.com>
2022-09-11 11:14:15 -0500

    Image Size Util Added

--- util/img/img_resize.py
+++ util/img/img_resize.py
@@ -1,6 +1,4 @@
 import os
 from pathlib import Path
 from PIL import Image
-
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-IMAGES_DIR = os.path.join(os.path.join(os.path.join(os.path.join(BASE_DIR, 'public'), 'assets'), 'images'), 'food')
+from config import IMAGES_DIR

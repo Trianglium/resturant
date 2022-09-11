c3f4ea277288610d6249862429a97a7156fcf4a4
Jordan <jbrockerrudow@gmail.com>
2022-09-11 07:47:08 -0500

    Initialize Menu Item Detail Component

---
+++ src/menu/MenuItem.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MenuItem(props) {
+    return(
+        <section className="menu-items">
+        <article className="pe-4">
+
+        </article>
+        <article className="pe-4">
+
+        </article>
+      </section>
+    )
+}
+
+export default MenuItem;

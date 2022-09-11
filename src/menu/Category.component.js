fd9f970d54195d5e9515a793ce084b8f2aa38200
Jordan <jbrockerrudow@gmail.com>
2022-09-11 07:46:54 -0500

    Initialize Menu Category Component

---
+++ src/menu/Category.component.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {CATEGORIES} from '../shared/menuCategories.js';
+
+function MenuCategory(props) {
+    return(
+        <div className='container'>
+            <div className='row'>
+                <div className='col-6 col-sm-4 col-md-3 col-lg-2'>
+
+                </div>
+                <div className='col-6 col-sm-4 col-md-3 col-lg-2'>
+                    
+                </div>
+                <div className='col-6 col-sm-4 col-md-3 col-lg-2'>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MenuCategory;

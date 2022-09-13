735b12bbec62fec115f17bf80c99854a2664557f
Jordan <jbrockerrudow@gmail.com>
2022-09-13 14:46:01 -0500

    V2.0.0/spa (#9)
    * Add theme colors
    
    * xlink:href  TO xlinkHref
    
    * Add Logo with details
    
    * Add Google Fonts to Core
    
    * Finish Menu HTML Template
    
    * Update Submit button font
    
    * Import core styles and add component styling
    
    * Add All Menu Items
    
    * Add Menu Page Header
    
    * Add Menu Categories section
    
    * Add Screenshot of site for progress
    
    * Create Party Menu Items Component and Template
    
    * Fix Stylsheet import

--- src/menu/Category.component.js
+++ src/menu/Category.component.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './Menu.component';
+import './Menu.component.css';
 import {CATEGORIES} from '../shared/menuCategories.js';
 
 function MenuCategory(props) {
     return(
+        <section id="menu-categories">
         <div className='container'>
             <div className='row'>
                 <div className='col-6 col-sm-4 col-md-3 col-lg-2'>
@@ -17,6 +18,7 @@ function MenuCategory(props) {
                 </div>
             </div>
         </div>
+        </section>
     )
 }
 
---
+++ src/menu/PartyMenuItem.component.js
@@ -0,0 +1,459 @@
+import React from './react';
+import './Menu.component.css';
+
+
+export default function PartyMenuItem() {
+    return(
+        <section id="party-items-detail">
+            <div>
+                <h2 className="category-name">
+                    Taste Catering
+                </h2>
+                <p className="category-description">
+                    Serves 20-25 people.
+                </p>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Combo 1 Party
+                                </h4>
+                                <p >5 lbs. Italian beef with gravy, hot and sweet peppers 5 loaf bread.</p>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $79.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Combo 2 Party
+                                </h4>
+                                <p >20 Italian sausages, 5 loafs bread, hot and sweet peppers, red sauce.</p>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $59.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Combo 3 Party
+                                </h4>
+                                <p >6 lbs. gyros meat with sauce, 20 pita bread, onions and tomatoes.</p>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $79.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Combo 4 Party
+                                </h4>
+                                <p >6 lbs. Chicken gyros with sauce, 20 pita bread, onions and tomatoes. Homemade hot sauce (red or green).</p>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $89.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Combo 5 Party
+                                </h4>
+                                <p >50 tacos steak, chicken or beef, flour or corn tortillas, tray of Mexican rice, tray of refried beans.</p>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $99.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h2 className="category-name">
+                    By The Pounds
+                </h2>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Italian Beef with Gravy Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $8.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Gyros with Sauce Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $8.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Chicken Gyros with Sauce Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $9.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h2 className="category-name">
+                    Sides
+                </h2>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    1 Loaf Bread Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $3.50
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Side of Hot Peppers Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $3.95
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Side of Sweet Peppers Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $3.75
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Side of Red Sauce Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $3.75
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Side of Gyros Sauce Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $3.75
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Pita Bread Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $0.85
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Chips and Guacamole Platter Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $27.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Chips and Salsa Platter Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $24.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Tray of Guacamole Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $18.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Tray of Refried Beans Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $19.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Tray of Garden Salad Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $19.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Tray of Mexican Rice Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $19.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <div>
+                        <div>
+                            <div className="menu-item-name">
+                                <h4>
+                                    Tray of Greek Salad Party
+                                </h4>
+                            </div>
+                            <div className="menu-item-price">
+                                <ul>
+                                    <li>
+                                    $27.99
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+};

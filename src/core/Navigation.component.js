18b98e463fcf76598b36b06086dc80bf56bbd8ce
Jordan <jbrockerrudow@gmail.com>
2022-09-14 12:38:17 -0500

    Trouble Shooting ChangeOnScroll Functionality

--- src/core/Navigation.component.js
+++ src/core/Navigation.component.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react'
 import '../landing/Landing.component.css';
 import {Link } from "react-router-dom";
 
@@ -8,28 +8,34 @@ export const FlexNav = () => {
     const changeNavbarColor = () =>{
         if(window.scrollY >= 80){
           setColorchange(true);
+          console.log(colorChange);
+          console.log('True',window.scrollY);
         }
         else{
           setColorchange(false);
+          console.log(colorChange);
+          console.log('False', window.scrollY);
         }
     };
     window.addEventListener('scroll', changeNavbarColor);
     return(
-        <nav className='sticky'>
-            <ul className="nav-flex-row">
-                <li className="nav-item">
-                    <Link to="/">About</Link>
-                </li>
-                <li className="nav-item">
-                    <Link to="/menu">Menu</Link>
-                </li>
-                <li className="nav-item">
-                <a href="#party-menu">Party Menu</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#contact">Contact</a>
-                </li>
-            </ul>
-        </nav>
+        <Fragment>
+            <nav className={colorChange ? 'navbar colorChange' : 'navbar'}>
+                <ul className="nav-flex-row">
+                    <li className="nav-item">
+                        <Link to="/">About</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/menu">Menu</Link>
+                    </li>
+                    <li className="nav-item">
+                    <a href="#party-menu">Party Menu</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </Fragment>
     )
 }

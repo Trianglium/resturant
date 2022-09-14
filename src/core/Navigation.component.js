c8038c86a5f4fb57a0a364d16daa99abe219dda4
Jordan <jbrockerrudow@gmail.com>
2022-09-14 13:14:28 -0500

    Add Background to Nav and remove router link

--- src/core/Navigation.component.js
+++ src/core/Navigation.component.js
@@ -1,29 +1,21 @@
-import React, { useState, Fragment } from 'react'
+import React from 'react'
 import '../landing/Landing.component.css';
-import {Link } from "react-router-dom";
+
+import Landing from '../landing/Landing.component';
+import Menu from '../menu/Menu.component';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 export const FlexNav = () => {
-    const [colorChange, setColorchange] = useState(false);
-
-    const changeNavbarColor = () =>{
-        if(window.scrollY >= 80){
-          setColorchange(true);
-          console.log(colorChange);
-          console.log('True',window.scrollY);
-        }
-        else{
-          setColorchange(false);
-          console.log(colorChange);
-          console.log('False', window.scrollY);
-        }
-    };
-    window.addEventListener('scroll', changeNavbarColor);
     return(
-        <Fragment>
-            <nav className={colorChange ? 'navbar colorChange' : 'navbar'}>
+            <nav>
                 <ul className="nav-flex-row">
                     <li className="nav-item">
-                        <Link to="/">About</Link>
+                        <Link to="/" hash="#about">About</Link>
                     </li>
                     <li className="nav-item">
                         <Link to="/menu">Menu</Link>
@@ -36,6 +28,5 @@ export const FlexNav = () => {
                     </li>
                 </ul>
             </nav>
-        </Fragment>
     )
 }

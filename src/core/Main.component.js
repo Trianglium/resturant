4128a2c0f630ec4cde3cba3ac21f219afbe75303
Jordan <jbrockerrudow@gmail.com>
2022-09-14 11:57:42 -0500

    Add Router and Module Components

--- src/core/Main.component.js
+++ src/core/Main.component.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import {FlexNav} from './Navigation.component';
+import Landing from '../landing/Landing.component';
+import Menu from '../menu/Menu.component';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route
+  } from "react-router-dom";
 
-function Main() {
+function Main(props) {
     return(
-        
-    )
+        <div className="fluid-container">
+            <Router>
+                <span>
+                <FlexNav />
+                <Switch>
+                    <Route path="/">
+                        <Landing />
+                    </Route>
+                    <Route path="/menu">
+                        <Menu />
+                    </Route>
+                </Switch>
+                </span>
+            </Router>
+
+
+        </div>
+    );
 }
+
 export default Main;

0e9132b6a93e7198c3fb8339c6433b9769492ebb
Jordan <jbrockerrudow@gmail.com>
2022-09-14 10:00:04 -0500

    V2.0.0/spa (#10)
    * Add theme colors
    
    * xlink:href  TO xlinkHref
    
    * Add Logo with details
    
    * Add Google Fonts to Core
    
    * Finish Menu HTML Template
    
    * Update Submit button font
    
    * Import core styles and add component styling
    
    * Add All Menu Items
    
    * Add Menu Page Header
    
    * Add Menu Categories section
    
    * Add Screenshot of site for progress
    
    * Create Party Menu Items Component and Template
    
    * Fix Stylsheet import
    
    * Create Menu Categories Mock Data

--- src/shared/menuCategories.js
+++ src/shared/menuCategories.js
@@ -1,6 +1,22 @@
-export const CATEGORIES = [
-    {
-        id: "",
-        name: ""
-    },
-];
+export const CATEGORIES = {
+    "party": [
+        { id: 1, name: "By The Pounds"},
+        { id: 2, name: "Sides"},
+        { id: 3, name: "Taste Catering"}
+    ],
+    "full": [
+        { id: 1, name: "Salads"},
+        { id: 2, name: "Gyros"},
+        { id: 3, name: "Beef"},
+        { id: 4, name: "Burgers"},
+        { id: 5, name: "Panini"},
+        { id: 6, name: "Hot Dogs"},
+        { id: 7, name: "Chicken"},
+        { id: 8, name: "Tacos and Burritos"},
+        { id: 9, name: "Dinners"},
+        { id: 10, name: "Kids Menu"},
+        { id: 11, name: "Sides"},
+        { id: 12, name: "Desserts"},
+        { id: 13, name: "Beverages"}
+    ]
+};

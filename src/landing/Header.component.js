854fab953377962cffc2c34e49317486c2f4ea54
Jordan <jbrockerrudow@gmail.com>
2022-09-14 12:04:28 -0500

    Separate Nav Component

--- src/landing/Header.component.js
+++ src/landing/Header.component.js
@@ -1,43 +1,7 @@
 import React from 'react';
 import './Landing.component.css';
 import Logo from '../logo';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
 
-export function FlexNav(props) {
-    return(
-        <Router>
-            <div>
-            <nav className='sticky'>
-                <ul className="nav-flex-row">
-                <li className="nav-item">
-                    <a href="#about">About</a>
-                </li>
-                <li className="nav-item">
-                    <Link to="/menu">Menu</Link>
-                </li>
-                <li className="nav-item">
-                <a href="#">Party Menu</a>
-                </li>
-                <li className="nav-item">
-                    <a href="#contact">Contact</a>
-                </li>
-                </ul>
-            </nav>
-
-            <Switch>
-            <Route path="/menu">
-            <Menu />
-            </Route>
-            </Switch>
-            </div>
-        </Router>
-    )
-}
 
 export function ImageHeader(props) {
     return(

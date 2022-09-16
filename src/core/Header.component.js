e387c5cbf8f7a9e1b6346a6526d2d44098d04ca5
Jordan <jbrockerrudow@gmail.com>
2022-09-16 09:22:40 -0500

    Relocate Header to Core Module

---
+++ src/core/Header.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../landing/Landing.component.css';
+import Logo from '../logo';
+
+
+export function ImageHeader(props) {
+    return(
+        <section className="section-intro">
+        <header className='section-intro-txt'>
+          <Logo />
+        </header>
+        <div className="link-to-book-wrapper">
+          <a className="btn btn-outline-light" role="button" href="tel:+16305520200"><strong>Order now</strong></a>
+        </div>
+      </section>
+    )
+}

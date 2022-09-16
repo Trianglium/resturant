cfd27a8afe6f1832e697c2b82b428f77b96662f1
Jordan <jbrockerrudow@gmail.com>
2022-09-16 10:32:31 -0500

    Add Yelp Review Data

---
+++ src/shared/reviews.js
@@ -0,0 +1,22 @@
+export const yelpReviews = [
+    {
+        feedback: "The food here is always amazing no matter what you order.",
+        name: "Adam W."
+    },
+    {
+        feedback: "Amazing food, number one place for takeout.",
+        name: "Angel Q."
+    },
+    {
+        feedback: "Love this place! I've never had a bad meal here.",
+        name: "Jim W."
+    },
+    {
+        feedback: "Great place for lunch or a quick bite to eat.",
+        name: "Brett B."
+    },
+    {
+        feedback: "This is our #1 lunch spot for work.",
+        name: "Jessica J."
+    }
+];

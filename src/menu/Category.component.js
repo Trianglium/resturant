4f557252ce391c893804f42169165c0834d0a32d
Jordan <jbrockerrudow@gmail.com>
2022-09-17 07:59:55 -0500

    Create MenuCategories Component and Category Function

--- src/menu/Category.component.js
+++ src/menu/Category.component.js
@@ -1,25 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Menu.component.css';
 import {CATEGORIES} from '../shared/menuCategories.js';
 
-function MenuCategory(props) {
-    return(
-        <section id="menu-categories">
-        <div className='container'>
-            <div className='row'>
-                <div className='col-6 col-sm-4 col-md-3 col-lg-2'>
-
-                </div>
-                <div className='col-6 col-sm-4 col-md-3 col-lg-2'>
-                    
-                </div>
-                <div className='col-6 col-sm-4 col-md-3 col-lg-2'>
-                    
-                </div>
+export const CategoryColumn = ({categoryName}) => {
+    if (!categoryName) {
+        return(
+            <div className='col-6 col-sm-4 col-md-3'></div>
+        )
+    }
+    else {
+        return(
+            <div className='col-6 col-sm-4 col-md-3'>
+                {categoryName}
             </div>
-        </div>
-        </section>
-    )
+        )
+    }
 }
 
+
+
+
+class MenuCategory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            fullMenuCategories: CATEGORIES.full,
+            partyMenuCategories: CATEGORIES.party
+        };
+    }
+    render() {
+        const fullCategory = this.state.fullMenuCategories.map((fullMenuCategory) => {
+            return(
+                <CategoryColumn categoryName={fullMenuCategory.name}/>
+            );
+        });
+        const partyCategory = this.state.partyMenuCategories.map((partyMenuCategory) => {
+            return(
+                <CategoryColumn categoryName={partyMenuCategory.name}/>
+            );
+        });
+        return(
+            <section id="menu-categories">
+                <div className='container'>
+                    <div className='row'>
+                        {fullCategory}
+                    </div>
+                    <div className='row'>
+                        {partyCategory}
+                    </div>
+                </div>
+            </section>
+        
+        );
+    }
+}
 export default MenuCategory;
